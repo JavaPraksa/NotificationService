0b8835c396d4f9a62c763fd281262a118f64fb1c
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-03-24 17:12:44 +0100

    initial project

---
+++ src/test/java/levi9/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package levi9.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
